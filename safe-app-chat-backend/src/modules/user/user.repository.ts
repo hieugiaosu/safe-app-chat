bcf093de5ab250eb34a217255c79afad1260c39a
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-12-15 22:48:09 +0700

    change to relative import for deployment

--- safe-app-chat-backend/src/modules/user/user.repository.ts
+++ safe-app-chat-backend/src/modules/user/user.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { BaseRepository } from 'src/common/base.repository';
+import { BaseRepository } from '../../common/base.repository';
 import { IUserRepository } from './user.interface';
 import { User } from './user.schema';
 
